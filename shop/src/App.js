afa457e69c88ae3c319cdfbf077d2740c0c9fa57
ihateindex <47144148+ihateindex@users.noreply.github.com>
2022-03-21 19:16:52 +0900

    0321 - import, export해서 데이터 사용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,9 +1,78 @@
 import './App.css';
+import {Navbar, Container, Nav, NavDropdown, Button} from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data';
 
 function App() {
+
+  let[shoes, shoes변경] = useState(data);
+  console.log(shoes);
+
   return (
     <div className="App">
-      <button type="button" class="btn btn-primary">Primary</button>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className='banner-main'>
+        <h1>20% Season Off</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button bsStyle="primary">Learn more</Button>
+        </p>
+      </div>
+      <div className='container'>
+        <div className='row'>
+          {/* <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="" width="100%"/>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content} & {shoes[0].price}</p>
+          </div>
+          <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="" width="100%"/>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="" width="100%"/>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div> */}
+          {shoes.map(function(shoe, idx) {
+            return(
+              <Product key={idx} id={shoe.id} title={shoe.title} content={shoe.content} price={shoe.price}></Product>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Product(props) {
+  return (
+    <div className='col-md-4'>
+      <img src={"https://codingapple1.github.io/shop/shoes"+(props.id+1)+".jpg"} alt="" width="100%"/>
+      <h4>{props.title}</h4>
+      <p>{props.content} & {props.price}</p>
     </div>
   );
 }
