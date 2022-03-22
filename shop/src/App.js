80a889805ff9c577cf9a8a76cf563c2fe4badeaa
ihateindex <47144148+ihateindex@users.noreply.github.com>
2022-03-22 18:02:46 +0900

    0322 - Link, Route, Switch 기능

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,68 +1,85 @@
 import './App.css';
-import {Navbar, Container, Nav, NavDropdown, Button} from 'react-bootstrap';
 import { useState } from 'react';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import data from './data';
+import Detail from './Detail';
+
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
+    let [shoes, shoes변경] = useState(data);
+    console.log(shoes);
 
-  let[shoes, shoes변경] = useState(data);
-  console.log(shoes);
-
-  return (
-    <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <div className='banner-main'>
-        <h1>20% Season Off</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
-        </p>
-        <p>
-          <Button bsStyle="primary">Learn more</Button>
-        </p>
-      </div>
-      <div className='container'>
-        <div className='row'>
-          {shoes.map(function(shoes, idx) {
-            return(
-              // * 기본
-              // <Card key={idx} id={shoes.id} title={shoes.title} content={shoes.content} price={shoes.price}></Card>
-              // * 축약형
-              <Card key={idx} idx={idx} shoes={shoes}></Card>
-            )
-          })}
+    return (
+        <div className="App">
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand>
+                        <Link to="/">ShoeShop</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link>
+                                <Link to="/">Home</Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to="/detail">Detail</Link>
+                            </Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Switch>
+                <Route exact path="/">
+                    <div className="banner-main">
+                        <h1>20% Season Off</h1>
+                        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                        <p>
+                            <Button bsStyle="primary">Learn more</Button>
+                        </p>
+                    </div>
+                    <div className="container">
+                        <div className="row">
+                            {shoes.map(function (shoes, idx) {
+                                return (
+                                    // * 기본
+                                    // <Card key={idx} id={shoes.id} title={shoes.title} content={shoes.content} price={shoes.price}></Card>
+                                    // * 축약형
+                                    <Card key={idx} idx={idx} shoes={shoes}></Card>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </Route>
+                <Route path="/detail">
+                    <Detail></Detail>
+                </Route>
+                <Route path="/:id">
+                    <div>아무거나 적었을때 이거 보여주셈</div>
+                </Route>
+            </Switch>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 function Card(props) {
-  return (
-    <div className='col-md-4'>
-      <img src={"https://codingapple1.github.io/shop/shoes"+(props.idx+1)+".jpg"} alt="" width="100%"/>
-      <h4>{props.shoes.title}</h4>
-      <p>{props.shoes.content} & {props.shoes.price}</p>
-    </div>
-  );
+    return (
+        <div className="col-md-4">
+            <img src={'https://codingapple1.github.io/shop/shoes' + (props.idx + 1) + '.jpg'} alt="" width="100%" />
+            <h4>{props.shoes.title}</h4>
+            <p>
+                {props.shoes.content} & {props.shoes.price}
+            </p>
+        </div>
+    );
 }
 
 export default App;
--- shop/src/index.js
+++ shop/src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// * 리액트 라우터 돔 import
+// * import할때 from 뒤에 경로가 없이 이름만 있다면 라이브러리임
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
